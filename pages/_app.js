c534aa1675a4b7fee5ba2c2dd4b22a31d9fdd9a2
Dennis Kimathi <kimathidennis38@gmail.com>
2022-07-06 12:38:10 +0300

    reusable logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import { Navbar, Footer } from '../components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className=" dark:bg-nft-dark bg-white min-h-screen ">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+
+    </div>
+    <Script src="https://kit.fontawesome.com/6749fd5a79.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+
+);
+
+export default MyApp;
