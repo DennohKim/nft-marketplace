4ebbcde7b44da21fcc9a405e800c64207bdb329e
Dennis Kimathi <kimathidennis38@gmail.com>
2022-07-10 22:07:26 +0300

    import navbar assets and hooks

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import images from '../assets';
 
 const Navbar = () => (
   <div>Navbar</div>
