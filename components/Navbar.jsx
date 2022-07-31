c06e30cbb575848945181db9542e54bb9d76e6c7
Dennis Kimathi <kimathidennis38@gmail.com>
2022-07-31 13:39:55 +0300

    style: make mobile responsive navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -61,6 +61,7 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const router = useRouter();
   const [active, setActive] = useState("Explore NFT's");
+  const [isOpen, setIsOpen] = useState(false);
 
   console.log({ theme });
 
@@ -99,6 +100,46 @@ const Navbar = () => {
         <div>
           <ButtonGroup setActive={setActive} router={router} />
         </div>
+      </div>
+      <div className="hidden md:flex ml-2 ">
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={20}
+            height={20}
+            alt="menu"
+            onClick={() => {
+              setIsOpen(false);
+            }}
+            className={theme === 'light' && 'filter invert'}
+          />
+        ) : (
+          <Image
+            src={images.menu}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="menu"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+            className={theme === 'light' && 'filter invert'}
+          />
+        )}
+
+        {isOpen && (
+          <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col ">
+            <div className="flex-1 p-4">
+              <MenuItems active={active} setActive={setActive} isMobile />
+
+            </div>
+            <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+              <ButtonGroup setActive={setActive} router={router} />
+
+            </div>
+          </div>
+        )}
 
       </div>
 
