9b22436a1cdc48d92336b65efc1ce4b595d9bbf8
Dennis Kimathi <kimathidennis38@gmail.com>
2022-08-01 12:52:13 +0300

    style: style banner

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,11 @@ const MyApp = ({ Component, pageProps }) => (
   <ThemeProvider attribute="class">
     <div className=" dark:bg-nft-dark bg-white min-h-screen ">
       <Navbar />
-      <Component {...pageProps} />
+      <div className="pt-65">
+        <Component {...pageProps} />
+
+      </div>
+
       <Footer />
 
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,8 @@ import { Banner } from '../components';
 const Home = () => (
   <div className="flex justify-center sm:px-4 p-12 ">
     <div className="w-full minwd:w-4/5">
-      <Banner />
+      <Banner parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl" childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left " name="Discover, collect, and sell extrordinary NFT's" />
     </div>
-    <h1 className="text-3xl font-bold underline">Hello World!</h1>
   </div>
 );
 
