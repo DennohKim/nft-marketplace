fb2ba1177e615da7d087a1099cab74e3e116e737
Dennis Kimathi <kimathidennis38@gmail.com>
2022-08-01 12:23:46 +0300

    feat: create responsive footer and style banner

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import { Banner } from '../components';
+
 const Home = () => (
-  <div>
+  <div className="flex justify-center sm:px-4 p-12 ">
+    <div className="w-full minwd:w-4/5">
+      <Banner />
+    </div>
     <h1 className="text-3xl font-bold underline">Hello World!</h1>
   </div>
 );
