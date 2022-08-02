9093e00766cc744cd8ff4257afa0ca50857a782f
Dennis Kimathi <kimathidennis38@gmail.com>
2022-08-02 08:39:30 +0300

    feat: create nftscard

--- components/index.js
+++ components/index.js
@@ -2,5 +2,8 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Button from './Button';
 import Banner from './Banner';
+import CreatorCard from './CreatorCard';
+import NFTCard from './NFTCard';
 
-export { Navbar, Footer, Button, Banner };
+
+export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard };
