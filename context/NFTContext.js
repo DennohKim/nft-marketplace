c692287733d5ccb61c750b1dc5c3a8970c594286
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-19 06:09:54 +0300

    feat: create context

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -1,11 +1,11 @@
-import { useState, useEffect, createContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3Modal from 'web3modal';
 import { ethers } from 'ethers';
 import axios from 'axios';
 
 import { MarketAddress, MarketAddressABI } from './constants';
 
-export const NFTContext = createContext();
+export const NFTContext = React.createContext();
 export const NFTProvider = ({ children }) => {
   const nftCurrency = 'MATIC';
 
